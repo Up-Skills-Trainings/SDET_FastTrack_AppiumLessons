bff75028461b6c259b8b4f6c073987a5bf79034d
OscarSah <oscar@cybertekschool.com>
2024-04-24 15:34:18 -0400

    cucumber started

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = false
+        dryRun = false,
         tags = "@wip"
 )
 public class CukesRunner {
