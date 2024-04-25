2573df4cb3d9098b97a6d35150c504071cec170a
OscarSah <oscar@cybertekschool.com>
2024-04-25 12:40:48 -0400

    saucelab remote test run

--- src/test/java/com/cydeo/utils/Driver.java
+++ src/test/java/com/cydeo/utils/Driver.java
@@ -3,6 +3,7 @@ package com.cydeo.utils;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.MutableCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -39,8 +40,32 @@ public class Driver {
                     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
                     break;
                 case "remote-android-sauceApp":
+                    MutableCapabilities caps = new MutableCapabilities();
+                    caps.setCapability("platformName", "Android");
+                    // change Application part
+                    caps.setCapability("appium:app", "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
+                    caps.setCapability("appium:deviceName", "Google Pixel 3 GoogleAPI Emulator");
+                    caps.setCapability("appium:platformVersion", "11.0");
+                    caps.setCapability("appium:automationName", "UiAutomator2");
+                    caps.setCapability("appPackage","com.swaglabsmobileapp");
+                    caps.setCapability("appActivity","com.swaglabsmobileapp.MainActivity");
+                    // add app activity and package
+                    MutableCapabilities sauceOptions = new MutableCapabilities();
+                    sauceOptions.setCapability("appiumVersion", "2.0.0");
+                    // username and access key is unique to the user
+                    sauceOptions.setCapability("username", "oauth-testermichael77-6fc8b");
+                    sauceOptions.setCapability("accessKey", "54923638-aed6-4432-8e2e-d8b72784bc1b");
+                    sauceOptions.setCapability("build", "sauceLab123");
+                    sauceOptions.setCapability("name", "smokeTest");
+                    sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
+                    caps.setCapability("sauce:options", sauceOptions);
 
-
+                    try {
+                        url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    driver = new AndroidDriver(url, caps);
                     break;
 
             }
--- src/test/resources/features/productTest.feature
+++ src/test/resources/features/productTest.feature
@@ -1,6 +1,11 @@
 Feature: Saucelab Mobile App Functionality
-@wip
-  Scenario: Test login and product page
+  Scenario: Test login and product page on local setup
     When User login to mobile app with "local" setup
+    And Add first product to cart
+    Then Verify product is added
+
+  @wip
+  Scenario: Test login and product page on local setup
+    When User login to mobile app with "remote" setup
     And Add first product to cart
     Then Verify product is added
